3372d366a86b6995dd5cc906adca5bed59c4882d
harshgitprojects <harshpatel21042002@gmail.com>
2024-05-13 23:23:40 +0530

    initial commit

---
+++ resources/views/main.blade.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="a/css/style.css">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+</head>
+<style>
+@media(max-width: 425px){
+   .main-about-box {
+    padding-top: 0;
+    padding: 20px;
+    display: flex;
+    margin: 30px;
+    align-items: stretch;
+    flex-direction: column;
+    img {
+        width: auto;
+        height: 300px;
+    }
+ }
+ .skills {
+    background: linear-gradient(45deg, #f30d5e, #9C27B0);
+    display: flex;
+    flex-direction: column;
+    height: 800px;
+    justify-content: center;
+    overflow-x: hidden;
+    align-items: center;
+ }
+ .skills-box1 {
+    box-shadow: 0 0 6px 1px black;
+    display: flex;
+    border-radius: 15px;
+    width: 300px;
+    height: 350px;
+    background: #0c0c0cc3;
+    color: #ffff;
+    flex-direction: column;
+    align-items: center;
+    margin: 25px;
+    font-size: larger;
+    backdrop-filter: blur(6px);
+  }
+   .Project-box-1 {
+        height: 500px;
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        align-items: center;
+    }
+    .Project-box{
+        width: 90%;
+    }
+   .footer-box1 {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+    width: auto;
+    margin: 25px;
+    flex-wrap: wrap;
+    }
+   .footer-box2 {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    color: white;
+    flex-wrap: nowrap;
+    flex-direction: column;
+  }
+
+
+}
+</style>
+<body style="background:black;" id="1">
+    <h1 style="text-align: center; color:white; ">site under maintenance</h1>
+    <div class="box1">
+       <div class="namebox">
+        <h1>Hii I Am HARSH </h1>
+        <h2>UI/UX Designer</h2>
+       </div>
+       <img class="img" src="a/image/img.jpg" data-aos="slide-left" data-aos-duration="8000"  data-aos-delay="50" data-aos-easing="ease-in-out" alt="">
+    </div>
+    {{-- <div class="box4">
+        <div class="box4-1"data-aos="slide-right"data-aos-easing="ease-in-out"data-aos-duration="4000"><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="60px" height="60px"><path fill="#FF3D00" d="M43.2,33.9c-0.4,2.1-2.1,3.7-4.2,4c-3.3,0.5-8.8,1.1-15,1.1c-6.1,0-11.6-0.6-15-1.1c-2.1-0.3-3.8-1.9-4.2-4C4.4,31.6,4,28.2,4,24c0-4.2,0.4-7.6,0.8-9.9c0.4-2.1,2.1-3.7,4.2-4C12.3,9.6,17.8,9,24,9c6.2,0,11.6,0.6,15,1.1c2.1,0.3,3.8,1.9,4.2,4c0.4,2.3,0.9,5.7,0.9,9.9C44,28.2,43.6,31.6,43.2,33.9z"/><path fill="#FFF" d="M20 31L20 17 32 24z"/></svg></div>
+        <div class="box4-1"data-aos="slide-right"data-aos-easing="ease-in-out"data-aos-duration="5000"><a href="https://www.linkedin.com/feed/"><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="60px" height="60px"><path fill="#0288D1" d="M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5V37z"/><path fill="#FFF" d="M12 19H17V36H12zM14.485 17h-.028C12.965 17 12 15.888 12 14.499 12 13.08 12.995 12 14.514 12c1.521 0 2.458 1.08 2.486 2.499C17 15.887 16.035 17 14.485 17zM36 36h-5v-9.099c0-2.198-1.225-3.698-3.192-3.698-1.501 0-2.313 1.012-2.707 1.99C24.957 25.543 25 26.511 25 27v9h-5V19h5v2.616C25.721 20.5 26.85 19 29.738 19c3.578 0 6.261 2.25 6.261 7.274L36 36 36 36z"/></svg></a></div>
+        <div class="box4-1"data-aos="slide-up"data-aos-easing="ease-in-out"data-aos-duration="6000"><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="60px" height="60px"><path fill="#03A9F4" d="M42,12.429c-1.323,0.586-2.746,0.977-4.247,1.162c1.526-0.906,2.7-2.351,3.251-4.058c-1.428,0.837-3.01,1.452-4.693,1.776C34.967,9.884,33.05,9,30.926,9c-4.08,0-7.387,3.278-7.387,7.32c0,0.572,0.067,1.129,0.193,1.67c-6.138-0.308-11.582-3.226-15.224-7.654c-0.64,1.082-1,2.349-1,3.686c0,2.541,1.301,4.778,3.285,6.096c-1.211-0.037-2.351-0.374-3.349-0.914c0,0.022,0,0.055,0,0.086c0,3.551,2.547,6.508,5.923,7.181c-0.617,0.169-1.269,0.263-1.941,0.263c-0.477,0-0.942-0.054-1.392-0.135c0.94,2.902,3.667,5.023,6.898,5.086c-2.528,1.96-5.712,3.134-9.174,3.134c-0.598,0-1.183-0.034-1.761-0.104C9.268,36.786,13.152,38,17.321,38c13.585,0,21.017-11.156,21.017-20.834c0-0.317-0.01-0.633-0.025-0.945C39.763,15.197,41.013,13.905,42,12.429"/></svg></div>
+        <div class="box4-1"data-aos="slide-left"data-aos-easing="ease-in-out"data-aos-duration="7000"><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="60px" height="60px"><path fill="#29b6f6" d="M24 4A20 20 0 1 0 24 44A20 20 0 1 0 24 4Z"/><path fill="#fff" d="M33.95,15l-3.746,19.126c0,0-0.161,0.874-1.245,0.874c-0.576,0-0.873-0.274-0.873-0.274l-8.114-6.733 l-3.97-2.001l-5.095-1.355c0,0-0.907-0.262-0.907-1.012c0-0.625,0.933-0.923,0.933-0.923l21.316-8.468 c-0.001-0.001,0.651-0.235,1.126-0.234C33.667,14,34,14.125,34,14.5C34,14.75,33.95,15,33.95,15z"/><path fill="#b0bec5" d="M23,30.505l-3.426,3.374c0,0-0.149,0.115-0.348,0.12c-0.069,0.002-0.143-0.009-0.219-0.043 l0.964-5.965L23,30.505z"/><path fill="#cfd8dc" d="M29.897,18.196c-0.169-0.22-0.481-0.26-0.701-0.093L16,26c0,0,2.106,5.892,2.427,6.912 c0.322,1.021,0.58,1.045,0.58,1.045l0.964-5.965l9.832-9.096C30.023,18.729,30.064,18.416,29.897,18.196z"/></svg></div>
+        <div class="box4-1"data-aos="slide-left"data-aos-easing="ease-in-out"data-aos-duration="8000"><img src="a/image/instagram.svg" width="60px" height="60px"></div>
+    </div> --}}
+    <a href="#1"><button class="BK"><img src="a/image/up.png" alt=""></button></a>
+    {{-- <img class="e" src="a/image/E1.svg" alt="">
+    <img class="e" src="a/image/E2.svg" alt=""> --}}
+    <div class="box2"style="">
+        <img src="a/image/img1.jpg" alt="">
+    </div>
+    <div class="nave-box">
+        <nav>
+            <a href="#1">HOME</a>
+            <a href="#2">ABOUT</a>
+            <a href="#3">PROJECT</a>
+            <a href="#4">CONTACT</a>
+        </nav>
+    </div>
+    <div class="main-about" id="2">
+        <div class="main-about-box">
+            <img src="a/image/about.png" alt=""data-aos="slide-right"data-aos-easing="ease-in-out">
+            <div class="main-about-box-contant">
+                <h1 data-aos="slide-left"data-aos-easing="ease-in-out">ABOUT ME</h1>
+                <p data-aos="slide-left"data-aos-easing="ease-in-out">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                <button data-aos="slide-up"data-aos-easing="ease-in-out">MORE...</button>
+            </div>
+        </div>
+    </div>
+    <div class="skills" id="" style="background:url('a/image/bg2.png');">
+        <div class="skills-box1" data-aos="slide-right" data-aos-offset="200" data-aos-duration="10000"data-aos-easing="ease-in-out">
+           <h3>Coding Skills</h3>
+           <div class="skill-box1-1"data-aos="slide-right" data-aos-duration="10000"data-aos-offset="200"data-aos-easing="ease-in-out">
+            <img src="a/image/css3-alt.png" alt="" data-aos="slide-right"data-aos-easing="ease-in-out">
+            <h4 data-aos="slide-right">CSS</h4>
+            <h4 data-aos="slide-right">100%</h4>
+           </div>
+           <div class="skill-box1-1"data-aos="slide-right" data-aos-duration="10000"data-aos-offset="200"data-aos-easing="ease-in-out">
+            <img src="a/image/js-square.png" alt="" data-aos="slide-right"data-aos-easing="ease-in-out">
+            <h4 data-aos="slide-right">Js</h4>
+            <h4 data-aos="slide-right">100%</h4>
+         </div>
+         <div class="skill-box1-1" data-aos="slide-right" data-aos-duration="10000"data-aos-offset="200"data-aos-easing="ease-in-out">
+            <img src="a/image/laravel.png" alt="" data-aos="slide-right" data-aos-duration="10000"data-aos-easing="ease-in-out">
+            <h4 data-aos="slide-right">Laravel</h4>
+            <h4 data-aos="slide-right">100%</h4>
+         </div>
+        </div>
+        <div class="skills-box1" data-aos="slide-left"data-aos-duration="10000" data-aos-offset="200"data-aos-easing="ease-in-out">   
+           <h3>Softwer Skills</h3>
+              <div class="skill-box1-1" data-aos="slide-left"data-aos-duration="10000"data-aos-easing="ease-in-out" data-aos-offset="200">
+                 <img src="a/image/figmaic.png" alt="" data-aos="slide-left">
+                 <h4 data-aos="slide-left">Figma</h4>
+                 <h4 data-aos="slide-left">100%</h4>
+              </div>
+              <div class="skill-box1-1" data-aos="slide-left" data-aos-duration="10000"data-aos-easing="ease-in-out" data-aos-offset="300">
+                <img src="a/image/Adobe.png" alt="" data-aos="slide-left">
+                <h4 data-aos="slide-left">Adobe</h4>
+                <h4 data-aos="slide-left">100%</h4>
+             </div>
+        </div>
+    </div>
+    <div class="Project" id="3">
+         <h2>PROJECTS</h2>
+        <div class="Project-box">
+            <div class="Project-box-1" data-aos="fade-left" data-aos-offset="200" data-aos-easing="ease-in-out">
+                <img src="a/image/project-1.jpg" alt="">
+                <h3>Project-1,2023</h3>
+                <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, fugit!</h4>
+            </div>
+            <div class="Project-box-1"data-aos="fade-right" data-aos-offset="200" data-aos-easing="ease-in-out">
+                <img src="a/image/project-2.png" alt="">
+                <h3>Project-1,2023</h3>
+                <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, fugit!</h4>
+            </div>
+            <div class="Project-box-1"data-aos="fade-left" data-aos-offset="200" data-aos-easing="ease-in-out">
+                <img src="a/image/project-3.jpg" alt="">
+                <h3>Project-1,2023</h3>
+                <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, fugit!</h4>
+            </div>
+            <div class="Project-box-1"data-aos="fade-right" data-aos-offset="200" data-aos-easing="ease-in-out">
+                <img src="a/image/project-1.jpg" alt="">
+                <h3>Project-1,2023</h3>
+                <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, fugit!</h4>
+            </div>
+        </div>
+    </div>
+    <div class="testimoniyal">
+        <div class="testimoniyal-box">
+           <div class="testimoniyal-box1">
+              <div class="img-box">
+                 <img src="" alt="">
+                  <h3></h3>
+              </div>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non voluptatibus quasi veritatis deleniti et illo!</p>
+           </div>
+           <div class="testimoniyal-box1">
+            <div class="img-box">
+               <img src="" alt="">
+                <h3></h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non voluptatibus quasi veritatis deleniti et illo!</p>
+         </div>
+        </div>
+    </div>
+    <div class="contact" id="4" style="background:url('a/image/bg3.jpg')">
+        <div class="contact-box">
+            <h2 data-aos="fade-up" data-aos-offset="200" data-aos-easing="ease-in-out">let's work together</h2>
+            <p data-aos="fade-up" data-aos-offset="300" data-aos-easing="ease-in-out">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi eos totam fugit provident unde atque accusamus illum voluptatum assumenda voluptas tempore debitis at architecto repellendus laborum ea facere officiis praesentium eligendi tenetur, similique, perspiciatis non! Nostrum dolorum sit, iusto a recusandae, numquam dignissimos nobis voluptatum facilis quaerat, molestias adipisci ducimus hic! Quae quos libero veniam veritatis natus provident, error ipsa alias earum facilis optio, rerum deleniti velit omnis pariatur molestiae accusantium eos cum tempora aut voluptates ullam laudantium! Veritatis asperiores repudiandae atque magnam eius et doloremque, laboriosam ex officiis recusandae debitis libero nam similique aliquam dolor quas doloribus sapiente maxime!</p>
+            <button data-aos="fade-up" data-aos-offset="200" data-aos-easing="ease-in-out">Contact</button>
+        </div>
+    </div>
+    <footer>
+        <div class="footer-box1">
+            <div class="box4-1"data-aos="slide-right"data-aos-easing="ease-in-out"data-aos-duration="4000"><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="60px" height="60px"><path fill="#FF3D00" d="M43.2,33.9c-0.4,2.1-2.1,3.7-4.2,4c-3.3,0.5-8.8,1.1-15,1.1c-6.1,0-11.6-0.6-15-1.1c-2.1-0.3-3.8-1.9-4.2-4C4.4,31.6,4,28.2,4,24c0-4.2,0.4-7.6,0.8-9.9c0.4-2.1,2.1-3.7,4.2-4C12.3,9.6,17.8,9,24,9c6.2,0,11.6,0.6,15,1.1c2.1,0.3,3.8,1.9,4.2,4c0.4,2.3,0.9,5.7,0.9,9.9C44,28.2,43.6,31.6,43.2,33.9z"/><path fill="#FFF" d="M20 31L20 17 32 24z"/></svg></div>
+     <div class="box4-1"data-aos="slide-right"data-aos-easing="ease-in-out"data-aos-duration="5000"><a href="https://www.linkedin.com/feed/"><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="60px" height="60px"><path fill="#0288D1" d="M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5V37z"/><path fill="#FFF" d="M12 19H17V36H12zM14.485 17h-.028C12.965 17 12 15.888 12 14.499 12 13.08 12.995 12 14.514 12c1.521 0 2.458 1.08 2.486 2.499C17 15.887 16.035 17 14.485 17zM36 36h-5v-9.099c0-2.198-1.225-3.698-3.192-3.698-1.501 0-2.313 1.012-2.707 1.99C24.957 25.543 25 26.511 25 27v9h-5V19h5v2.616C25.721 20.5 26.85 19 29.738 19c3.578 0 6.261 2.25 6.261 7.274L36 36 36 36z"/></svg></a></div>
+     <div class="box4-1"data-aos="slide-up"data-aos-easing="ease-in-out"data-aos-duration="6000"><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="60px" height="60px"><path fill="#03A9F4" d="M42,12.429c-1.323,0.586-2.746,0.977-4.247,1.162c1.526-0.906,2.7-2.351,3.251-4.058c-1.428,0.837-3.01,1.452-4.693,1.776C34.967,9.884,33.05,9,30.926,9c-4.08,0-7.387,3.278-7.387,7.32c0,0.572,0.067,1.129,0.193,1.67c-6.138-0.308-11.582-3.226-15.224-7.654c-0.64,1.082-1,2.349-1,3.686c0,2.541,1.301,4.778,3.285,6.096c-1.211-0.037-2.351-0.374-3.349-0.914c0,0.022,0,0.055,0,0.086c0,3.551,2.547,6.508,5.923,7.181c-0.617,0.169-1.269,0.263-1.941,0.263c-0.477,0-0.942-0.054-1.392-0.135c0.94,2.902,3.667,5.023,6.898,5.086c-2.528,1.96-5.712,3.134-9.174,3.134c-0.598,0-1.183-0.034-1.761-0.104C9.268,36.786,13.152,38,17.321,38c13.585,0,21.017-11.156,21.017-20.834c0-0.317-0.01-0.633-0.025-0.945C39.763,15.197,41.013,13.905,42,12.429"/></svg></div>
+            <div class="box4-1"data-aos="slide-left"data-aos-easing="ease-in-out"data-aos-duration="7000"><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="60px" height="60px"><path fill="#29b6f6" d="M24 4A20 20 0 1 0 24 44A20 20 0 1 0 24 4Z"/><path fill="#fff" d="M33.95,15l-3.746,19.126c0,0-0.161,0.874-1.245,0.874c-0.576,0-0.873-0.274-0.873-0.274l-8.114-6.733 l-3.97-2.001l-5.095-1.355c0,0-0.907-0.262-0.907-1.012c0-0.625,0.933-0.923,0.933-0.923l21.316-8.468 c-0.001-0.001,0.651-0.235,1.126-0.234C33.667,14,34,14.125,34,14.5C34,14.75,33.95,15,33.95,15z"/><path fill="#b0bec5" d="M23,30.505l-3.426,3.374c0,0-0.149,0.115-0.348,0.12c-0.069,0.002-0.143-0.009-0.219-0.043 l0.964-5.965L23,30.505z"/><path fill="#cfd8dc" d="M29.897,18.196c-0.169-0.22-0.481-0.26-0.701-0.093L16,26c0,0,2.106,5.892,2.427,6.912 c0.322,1.021,0.58,1.045,0.58,1.045l0.964-5.965l9.832-9.096C30.023,18.729,30.064,18.416,29.897,18.196z"/></svg></div>
+            <div class="box4-1"data-aos="slide-left"data-aos-easing="ease-in-out"data-aos-duration="8000"><img src="a/image/instagram.svg" width="60px" height="60px"></div>
+        </div>
+        <div class="footer-box2">
+            <a href="#1">HOME</a>
+            <a href="#2">ABOUT</a>
+            <a href="#3">PROJECT</a>
+            <a href="#4">CONTACT</a>  
+        </div>
+    </footer>
+    <div class="copyrighty">
+        <img src="a/image/copyrights.png" alt="">
+        <h3>-harshpatel</h3>
+    </div>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script src="a/js/app.js"></script>
+    <script>
+      AOS.init();
+    </script>
+</body>
+</html>
